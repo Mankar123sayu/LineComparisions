1bd0490b955935fd53b486628d3caea5e8a73a9a
Mankar123sayu <sayumankar77@gmail.com>
2023-05-11 19:38:07 +0530

    Sayunkta

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LineComparisions
+{
+    public class Operation
+    {
+     public static void Main(string[] args)
+        {
+            Comparision Project = new Comparision();
+            Project.LengthOfLine();
+        }
+    }
+}
