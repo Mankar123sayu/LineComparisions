d1ba715dc582f019bf333baa4418dca6588fe2c8
Mankar123sayu <125425650+Mankar123sayu@users.noreply.github.com>
2023-05-11 19:58:50 +0530

    Update Comparision.cs

--- Comparision.cs
+++ Comparision.cs
@@ -8,24 +8,6 @@ namespace LineComparisions
 {
     public class Comparision
     {
-        public void LengthOfLine()
-        {
-            Console.Write("Enter the value of x1: ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the value of y1: ");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the value of x2: ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the value of y2: ");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-
-            //Length of Line = sqrt[(x2-x1)^2 + (y2-y1)^2]
-            double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine(result);
-        }
         public void EqualityCheck(double line1, double line2)
         {
             if (line1.Equals(line2))
